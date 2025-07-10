9dd61fdaa392c1f8671a8ce59a84278f0067fa7e
ColdZero777 <143316713+ColdZero777@users.noreply.github.com>
2025-07-10 20:13:08 +0900

    로고 추가
    근데 사이드바 껐을때 없어지는건 아직...

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import pandas as pd
-import folium
-import streamlit_folium as sf
-
-st.set_page_config(layout="wide")
 
 st.title('To Do Safe')
+st.set_page_config(layout="wide")
 
 # 웹 사이트 탭
 tab1, tab2, tab3 = st.tabs(['TDS 사이트란?', '소방 안전 지도', 'To Do Safe Your Mission!'])
@@ -14,33 +11,34 @@ with tab1:
     st.header('To do Safe : 미션을 깨면서 안전을 점검하세요')
     # 클릭시 내용 보여주는 
     with st.expander('TDS(To Do Safe) 사이트란?'):
-        st.write(
-        '\n소방안전에 대한 경각심을 깨워주고 점검하는 습관을 들일 수 있게 도와주는 사이트입니다.' +
-        '\n집안에서 사건사고가 발생하지 않도록 미리 점검하며 미션을 클리어하고 포인트를 흭득하세요!')
+        st.write('To Do Safe 사이트' +
+        '\n시민들의 소방안전에 대한 경각심을 깨워주고 점검하는 습관을 들일 수 있게 도와주는 사이트입니다.' +
+        '\n집안에서 발생할 수 있는 사건들을 점검하며 미션을 클리어하고 포인트를 흭득하세요!')
 
 with tab2:
     st.header('소방 안전 지도')
-    data = pd.read_csv("서울시 소방서 위치정보.csv", encoding='utf-8')
-    m = folium.Map(location=[37.5665, 126.9780], zoom_start=12)
+    # 위도와 경도를 활용하여 지도 이미지를 사이트 안에 송출
+    data = pd.DataFrame({
+        'latitude': [37.7749, 34.0522, 40.7128],
+        'longitude': [-122.4194, -118.2437, -74.0060]
+    })
+    # 맵 이미지 송출
+    st.map(data)
 
-    # 소방서 위치 마커 추가
-    for i in data.index:
-        name = data.loc[i, '서ㆍ센터명']
-        lat = data.loc[i, '위도']
-        lon = data.loc[i, '경도']
-        folium.Marker(
-            location=[lat, lon],
-            popup=name,
-            icon=folium.Icon(color='red', icon='info-sign')
-        ).add_to(m)
-
-    # st_folium으로 지도 출력
-    st_data = sf.st_folium(m, width=1000, height=500)
-    
 with tab3:
     st.header('To Do Safe Your Mission!')
     st.write("제작 예정")
+    
+# 웹 로고
+logoUrl = "Logo.png"
+logo = st.logo(logoUrl)
+st.html("""
+  <style>
+    [alt=Logo] {
+      height: 4rem;
+    }
+  </style>
+        """)
 
 # 웹 사이트 사이드 바
 search = st.sidebar.text_input('검색할 내용을 입력하세요.')
-
