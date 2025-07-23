fac1cec6cd61de1080f9084766751cc96aedbcd4
김종훈 <133855697+JanMatny327@users.noreply.github.com>
2025-07-24 01:27:34 +0900

    Update 3_📺️영상 수료, 포인트 GET!.py

--- pages/3_📺️영상 수료, 포인트 GET!.py
+++ pages/3_📺️영상 수료, 포인트 GET!.py
@@ -6,7 +6,39 @@ if 'points' not in st.session_state:
 if 'watched' not in st.session_state:
     st.session_state.watched = False
 
-st.title("영상 완료 감지 및 포인트 지급")
+# --- 페이지 설정 ---
+st.set_page_config(layout="wide")
+
+if not st.session_state.get("logged_in"):
+    st.warning("⚠️ 로그인 후 이용 가능합니다.")
+    st.stop()
+
+st.set_page_config(page_title="소방 안전 지도", page_icon="🗺️")
+
+with st.sidebar:
+    st.image("https://raw.githubusercontent.com/JanMatny327/bigData_congress/main/TDS_일개미들.png", width=150) # 로고를 사이드바 상단에 배치
+    st.markdown("""
+        <style>
+            [alt=Logo] {
+                height: 4rem!important;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+    
+    if st.session_state.logged_in:
+        st.success(f"환영합니다, {st.session_state.username}님!")
+        
+        if st.button("로그아웃"):
+            st.session_state.logged_in = False
+            st.session_state.username = ""
+            # 로그아웃 시 입력 필드 초기화 (필요하다면)
+            st.session_state.login_username_input = ""
+            st.session_state.login_password_input = ""
+            st.session_state.new_username_input = ""
+            st.session_state.new_password_input = ""
+            st.rerun() # 로그아웃 후 페이지 새로고침
+
+st.title("영상 수강")
 
 video_html = """
 <video id="video" width="640" height="360" controls>
