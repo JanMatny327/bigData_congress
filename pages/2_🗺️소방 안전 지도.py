48a43065428fcb7f17777f2c768713885b720c40
김종훈 <133855697+JanMatny327@users.noreply.github.com>
2025-07-24 16:32:50 +0900

    Update 2_🗺️소방 안전 지도.py

--- pages/2_🗺️소방 안전 지도.py
+++ pages/2_🗺️소방 안전 지도.py
@@ -49,7 +49,7 @@ with tab1:
 
     try:
         data = pd.read_csv("https://raw.githubusercontent.com/JanMatny327/bigData_congress/main/pages/seoul_119_data.csv")
-
+        data2 = pd.read_csv("https://raw.githubusercontent.com/JanMatny327/bigData-congress/main/pages/소방청_시도 소방서 현황5.csv")
         # --- 내 위치 가져오기 ---
         location = get_geolocation()
         if location:
@@ -71,22 +71,57 @@ with tab1:
         ).add_to(m)
 
         # 소방서 마커
-        for _, row in data.iterrows():
-            popup_html = f"""
-                <div style="width:200px">
-                    <b>소방서 명:</b> {row['소방서이름 ']}<br>
-                    <b>주소:</b> {row['소방서주소']}<br>
-                    <b>전화번호:</b> {row['전화번호']}<br>
-                    <img src="{row['소방서_이미지_주소']}" width="300px">
-                </div>"""
-            popup = folium.Popup(folium.IFrame(popup_html, width=355, height=310), max_width=355)
-            icon = CustomIcon("https://raw.githubusercontent.com/JanMatny327/bigData_congress/main/소방서.png", icon_size=(40, 40))
-            folium.Marker(
-                location=[row['위도'], row['경도']],
-                tooltip=row['소방서이름 '],
-                popup=popup,
-                icon=icon
-            ).add_to(m)
+    for i in data2.index:
+        name = data2.loc[i, '소방서']
+        lat = float(data2.loc[i, '위도'])
+        lon = float(data2.loc[i, '경도'])
+        address = (data2.loc[i, '주소'])
+        number = (data2.loc[i, '전화번호'])
+        image = "https://cdn-icons-png.flaticon.com/512/2801/2801574.png"
+        popup_html = f"""
+        <div style=width:"200px">
+            <b>소방서 명:</b> {name}<br>
+            <b>소방서 주소:</b> {address}<br>
+            <b>소방서 전화번호:</b> {number}<br>
+            <img src="{image}" width="300px">
+        </div>
+        """
+        tooltip = name
+        popup_text = f"소방서 명: {name}<br>소방서 주소: {address}<br>소방서 전화번호:</b> {number}<br>"
+        popup = folium.Popup(folium.IFrame(popup_html, width=355, height=310), max_width=355)
+        icon = CustomIcon("소방서.png", icon_size=(40, 40))
+        folium.Marker(
+            location=[lat, lon],
+            tooltip=tooltip,
+            popup=popup,
+            icon=icon
+        ).add_to(m)
+    for i in data.index:
+        name = data.loc[i, '소방서이름 ']
+        lat = data.loc[i, '위도']
+        lon = data.loc[i, '경도']
+        address = data.loc[i, '소방서주소']
+        number = data.loc[i,'전화번호']
+        url = data.loc[i, '소방서 이미지 주소']
+        image_url = f"{url}"
+        popup_html = f"""
+        <div style=width:"200px">
+            <b>소방서 명:</b> {name}<br>
+            <b>소방서 주소:</b> {address}<br>
+            <b>소방서 전화번호:</b> {number}<br>
+            <img src="{image_url}" width="300px">
+        </div>
+        """
+        tooltip = name
+        popup_text = f"소방서 명: {name}<br>소방서 주소: {address}<br>소방서 전화번호:</b> {number}<br>"
+        popup = folium.Popup(folium.IFrame(popup_html, width=355, height=310), max_width=355)
+        icon = CustomIcon("소방서.png", icon_size=(40, 40))
+        folium.Marker(
+            location=[lat, lon],
+            tooltip=tooltip,
+            popup=popup,
+            icon=icon
+        ).add_to(m)
 
         sf.st_folium(m, width=1920, height=600)
 
