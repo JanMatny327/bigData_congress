4b3e3f586867a3bf86133d23d7110a34d03cc874
김종훈 <133855697+JanMatny327@users.noreply.github.com>
2025-07-24 17:17:42 +0900

    Update 2_🗺️소방 안전 지도.py

--- pages/2_🗺️소방 안전 지도.py
+++ pages/2_🗺️소방 안전 지도.py
@@ -122,7 +122,7 @@ with tab1:
                 icon=icon
             ).add_to(m)
 
-        sf.st_folium(m, width=1920, height=600)
+    sf.st_folium(m, width=1920, height=600)
 
     except Exception as e:
         st.error(f"🚨 지도 로딩 중 오류가 발생했습니다: {e}")
