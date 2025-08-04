7fd39009c5c167e09ec153b7060d20c7776f96ed
김종훈 <133855697+JanMatny327@users.noreply.github.com>
2025-08-04 16:24:03 +0900

    Update 2_🗺️소방 안전 지도.py

--- pages/2_🗺️소방 안전 지도.py
+++ pages/2_🗺️소방 안전 지도.py
@@ -50,7 +50,7 @@ with tab1:
     try:
         data = pd.read_csv("https://raw.githubusercontent.com/JanMatny327/bigData_congress/main/pages/seoul_119_data.csv")
         data2 = pd.read_csv("https://raw.githubusercontent.com/JanMatny327/bigData_congress/5383d52756a325ed369f401fb521aac43b3e3865/fire_station_status_v5.csv")
-        result = pd.concat([data, data2], ignore=True)
+        result = pd.concat([data, data2], ignore_index=True)
         districts = sorted(result['본부명'].dropna().unique())
 
         col1 = st.columns(1)[0]
